e1dd7db54e12e4e35f8dae05cfcb09560381a337
Tedu <1421534383@qq.com>
2019-11-19 10:39:26 +0800

    init save

---
+++ exercise04.py
@@ -0,0 +1,49 @@
+# class Person:
+#     def __init__(self,name):
+#         self.name = name
+#
+#     def go_to(self,position,type):
+#         '''
+#         :param position:地名
+#         :param type: 去的方法
+#         :return:
+#         '''
+#         print('去：'+position)
+#         type.run()
+#
+# class Car:
+#     def run(self):
+#         print('走你～')
+#
+# class Airplane:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# c01 = Car()
+# lz = Person('老张')
+# lz.go_to('东北', c01)
